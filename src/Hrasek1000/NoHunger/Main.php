75068d7db243d563df8ba94b82e2b5deef4aa865
Hrasek1000cz <69074536+Hrasek1000cz@users.noreply.github.com>
2020-08-02 10:22:28 +0200

    Main.php for NoHunger

--- src/Hrasek1000/NoHunger/Main.php
+++ src/Hrasek1000/NoHunger/Main.php
@@ -1,1 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hrasek1000\NoHunger;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerExhaustEvent;
+
+class Main extends PluginBase implements Listener{
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function Hunger(PlayerExhaustEvent $exhaustEvent) {
+        $exhaustEvent->setCancelled(true);
+    }
+}
 
